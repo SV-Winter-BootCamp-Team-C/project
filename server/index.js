20572018cc5b296a626515ff54bd34ad602c0e49
ssojungg <kawy.sojung@gmail.com>
2023-12-28 12:50:23 +0900

    chore: 초기세팅

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const app = express();                //app에 사용할 express함수 불러오
 const port = 8000;  //원하는 포트로(3000,4000,5000)
 
 app.get('/',(req,res) => {
-  res.send('there');
+    res.send('there');
 })
 
 app.listen(port,() => {
-  console.log(`port 8000 start`)
+    console.log(`port 8000 start`)
 })
