dcc9462cbeaa96a8c594094331f6e0fba005cfea
ssojungg <kawy.sojung@gmail.com>
2024-01-05 12:16:41 +0900

    chore: 데이터 베이스 연동 및 docker 초기 세팅 변경
    docker-compose up --build만 치고 코드 수정하고Controller+s (저장)만 하면 코드 자동 재시작함

---
+++ models/db.sql
@@ -0,0 +1,80 @@
+-- ENUM 타입 정의
+DROP TYPE IF EXISTS question_type CASCADE;
+CREATE TYPE question_type AS ENUM (
+    'MULTIPLE_CHOICE',
+    'SUBJECTIVE_QUESTION',
+    'CHECKBOX',
+    'DROPDOWN'
+);
+
+
+-- User 테이블 생성
+CREATE TABLE IF NOT EXISTS "User" (
+    "id" SERIAL PRIMARY KEY,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "name" VARCHAR(20) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
+);
+
+-- Survey 테이블 생성
+CREATE TABLE IF NOT EXISTS "Survey" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL,
+    "title" VARCHAR(50) NOT NULL,
+    "url" VARCHAR(500) NOT NULL,
+    "font" VARCHAR(50) NOT NULL,
+    "color" VARCHAR(50) NOT NULL,
+    "button_style" VARCHAR(50) NOT NULL,
+    "main_image_url" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP,
+    "deadline" TIMESTAMP,
+    FOREIGN KEY ("user_id") REFERENCES "User"("id")
+);
+
+-- Question 테이블 생성
+CREATE TABLE IF NOT EXISTS "Question" (
+    "id" SERIAL PRIMARY KEY,
+    "survey_id" INT NOT NULL,
+    "type" question_type NOT NULL,
+    "content" VARCHAR(100) NOT NULL,
+    "image_url" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP,
+    FOREIGN KEY ("survey_id") REFERENCES "Survey"("id")
+);
+
+-- Choice 테이블 생성
+CREATE TABLE IF NOT EXISTS "Choice" (
+    "id" SERIAL PRIMARY KEY,
+    "question_id" INT NOT NULL,
+    "option" VARCHAR(100) NOT NULL,
+    "count" INT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP,
+    FOREIGN KEY ("question_id") REFERENCES "Question"("id")
+);
+
+-- Answer 테이블 생성
+CREATE TABLE IF NOT EXISTS "Answer" (
+    "id" SERIAL PRIMARY KEY,
+    "question_id" INT NOT NULL,
+    "user_id" INT NOT NULL,
+    "sub_content" VARCHAR(500),
+    "obj_content" VARCHAR(500),
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP,
+    FOREIGN KEY ("question_id") REFERENCES "Question"("id"),
+    FOREIGN KEY ("user_id") REFERENCES "User"("id")
+);
+
+
+
+
