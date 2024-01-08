576ca627c5ef08281422b95b1191ab64fcd6d483
Heeseon Jang <loedy1123@gmail.com>
2024-01-09 04:45:45 +0900

    Revert "feat: 설문 내용 보기 api"
    This reverts commit d5ed2ca8886dd7fbf5e382c2b72cf5765de87779.

--- routers/surveyRouter.js
+++ routers/surveyRouter.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const surveyController = require('../controller/surveyCreate');
-const surveyGetController = require('../controller/surveyReadById');
 
 router.post('/surveys', surveyController.createSurveyWithQuestionsAndChoices);
-router.get('/surveys/:id', surveyGetController.getSurveyById);
+
 module.exports = router;
