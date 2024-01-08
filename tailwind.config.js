221d5b67c20e1b57e8ec59f743a13d9f4a707e13
EunJu JO <dmswnehf@naver.com>
2024-01-09 04:54:11 +0900

    design: tailwind config 파일 작성

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ export default {
   theme: {
     extend: {
       // 메인 및 서브 색상 지정
-      colors: {},
-      fontFamily: {
-        sans: ['sans-serif'],
+      colors: {
+        darkPurple: '#66629F',
+        purple: '#918DCA',
+        skyBlue: '#A3C9F0',
+        darkGary: '#8E8E8E',
+        gray: '#B4B4B4',
+        lightGray: '#D9D9D9',
+        white: '#FFFFFF',
       },
     },
   },
