38ea8808853b4365a861054cf1415ac25df7d358
ssojungg <kawy.sojung@gmail.com>
2024-01-09 22:23:23 +0900

    fix: 질문 생성 api 수정

--- controller/surveyCreate.js
+++ controller/surveyCreate.js
@@ -1,51 +1,92 @@
-const { pg } = require('../models/database');
+const { sequelize } = require('../models');
+const { Survey, Question, Choice } = require('../models');
 
 const createSurveyWithQuestionsAndChoices = async (req, res) => {
+  const t = await sequelize.transaction();
+
   try {
-    const { user_id, title, open, font, color, button_style, main_image_url, deadline, questions } = req.body;
+    const {
+      userId,
+      title,
+      description,
+      open,
+      font,
+      color,
+      buttonStyle,
+      mainImageUrl,
+      deadline,
+      questions,
+    } = req.body;
 
-    console.log('Request body:', req.body); // 요청 본문 로그 찍기
+    // surveyUrl을 미리 정의
+    const surveyUrl = `http://yourwebsite.com/survey/placeholder`;
 
-    await pg.query('BEGIN');
-
-    const defaultUrl = 'http://example.com/survey';
-
-    const surveyResult = await pg.query(
-      'INSERT INTO "Survey" (user_id, title, open, url, font, color, button_style, main_image_url, deadline) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
-
-      [user_id, title, open, defaultUrl, font, color, button_style, main_image_url, deadline],
+    const survey = await Survey.create(
+      {
+        userId,
+        title,
+        description,
+        open,
+        url: surveyUrl,
+        font,
+        color,
+        buttonStyle,
+        mainImageUrl,
+        deadline,
+      },
+      { transaction: t },
     );
 
-    console.log('Survey Insert Result:', surveyResult.rows[0]); // Survey 삽입 결과 로그 찍기
+    const surveyId = survey.id;
+    const updatedSurveyUrl = `http://yourwebsite.com/survey/${surveyId}`;
 
-    const surveyId = surveyResult.rows[0].id;
+    await Survey.update(
+      { url: updatedSurveyUrl },
+      {
+        where: { id: surveyId },
+        transaction: t,
+      },
+    );
 
     for (const question of questions) {
-      console.log(`${question.type}`);
-      if (!['MULTIPLE_CHOICE', 'SUBJECTIVE_QUESTION', 'CHECKBOX', 'DROPDOWN'].includes(question.type)) {
+      if (
+        ![
+          'MULTIPLE_CHOICE',
+          'SUBJECTIVE_QUESTION',
+          'CHECKBOX',
+          'DROPDOWN',
+        ].includes(question.type)
+      ) {
         throw new Error('Invalid question type');
       }
 
-      const questionResult = await pg.query(
-        'INSERT INTO "Question" (survey_id, type, content, image_url) VALUES ($1, $2, $3, $4) RETURNING id',
-        [surveyId, question.type, question.content, question.image_url],
+      const newQuestion = await Question.create(
+        {
+          surveyId,
+          type: question.type,
+          content: question.content,
+          imageUrl: question.imageUrl,
+        },
+        { transaction: t },
       );
-      const questionId = questionResult.rows[0].id;
 
-      console.log('Question Insert Result:', questionResult.rows[0]);
-
-      if (question.choices) {
+      if (question.choices && question.choices.length > 0) {
         for (const choice of question.choices) {
-          console.log('Inserting choice:', choice);
-          await pg.query('INSERT INTO "Choice" (question_id, option) VALUES ($1, $2)', [questionId, choice.option]);
+          await Choice.create(
+            {
+              questionId: newQuestion.id,
+              option: choice.option,
+            },
+            { transaction: t },
+          );
         }
       }
     }
 
-    await pg.query('COMMIT');
+    await t.commit();
     res.status(201).json({ message: '설문 생성 완료', surveyId: surveyId });
   } catch (error) {
-    await pg.query('ROLLBACK');
+    await t.rollback();
     res.status(500).json({ message: '설문 생성 오류', error: error.message });
   }
 };
