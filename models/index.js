55ce13602a8cc48d1c04c81074581af5b6da9fc2
ssojungg <kawy.sojung@gmail.com>
2024-01-09 22:36:35 +0900

    fix: 데이터베이스 관계 설정

--- models/index.js
+++ models/index.js
@@ -8,10 +8,19 @@ const Choice = require('./choice')(sequelize, Sequelize.DataTypes);
 const Answer = require('./answer')(sequelize, Sequelize.DataTypes);
 
 // 모델 간 관계 설정
-Question.belongsTo(Survey, { foreignKey: 'surveyId' }); // Survey와 Question 관계 설정
-Question.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Survey, { foreignKey: 'userId' }); // User와 Survey 간의 관계 설정
+Survey.belongsTo(User, { foreignKey: 'userId' });
+
+Survey.hasMany(Question, { foreignKey: 'surveyId' }); // Survey와 Question 간의 관계 설정
+Question.belongsTo(Survey, { foreignKey: 'surveyId' });
+
+Question.hasMany(Choice, { foreignKey: 'questionId' }); // Question과 Choice 간의 관계 설정
 Choice.belongsTo(Question, { foreignKey: 'questionId' });
+
+Question.hasMany(Answer, { foreignKey: 'questionId' }); // Question과 Answer 간의 관계 설정
 Answer.belongsTo(Question, { foreignKey: 'questionId' });
+
+User.hasMany(Answer, { foreignKey: 'userId' }); // User와 Answer 간의 관계 설정
 Answer.belongsTo(User, { foreignKey: 'userId' });
 
 module.exports = {
