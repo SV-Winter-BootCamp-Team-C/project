952a883a4f1ed3dfbe35da1ff40623930f8ba4ea
Heeseon Jang <loedy1123@gmail.com>
2024-01-09 16:42:30 +0900

    feat : 설문 내용 보기 api

--- routers/surveyRouter.js
+++ routers/surveyRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const surveyController = require('../controller/surveyCreate');
 const surveyAllUserController = require('../controller/formAllUser');
+const surveyGetController = require('../controller/surveyContentRead');
 
 router.post('/surveys', surveyController.createSurveyWithQuestionsAndChoices);
 router.get('/:id/forms', surveyAllUserController.getUserSurveys);
+router.get('/surveys/:id', surveyGetController.getSurveyById);
 
 module.exports = router;
