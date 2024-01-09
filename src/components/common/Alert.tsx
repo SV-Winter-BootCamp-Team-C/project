be6a355b29f782f1abfd2fe2f11e2949e20006db
EunJu JO <dmswnehf@naver.com>
2024-01-10 04:46:24 +0900

    design: 알림창, navbar 퍼블리싱

---
+++ src/components/common/Alert.tsx
@@ -0,0 +1,39 @@
+import close from '@/assets/closebtn.svg';
+import { useState } from 'react';
+
+interface AlertProps {
+  message: string;
+  buttonText: string | null;
+}
+
+function Alert({ message, buttonText }: AlertProps) {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  const closeAlert = () => {
+    setIsVisible(false);
+  };
+
+  if (!isVisible) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20">
+      <div className="relative h-40 w-80 flex flex-col items-center justify-center bg-white shadow-md rounded-[1.25rem]">
+        <div className="absolute right-2 top-2">
+          <div className="flex items-center justify-center w-6 h-6 cursor-pointer" onClick={closeAlert}>
+            <img src={close} alt="close" className="w-2 h-2" />
+          </div>
+        </div>
+        {message}
+        <button
+          type="submit"
+          onClick={closeAlert}
+          className="w-[6.25rem] h-9 rounded-[0.625rem] bg-purple text-base text-white mt-8"
+        >
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Alert;
