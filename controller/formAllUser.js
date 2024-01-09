f3c9ad892e782eaf3753536c1e5b30b05613baec
ssojungg <kawy.sojung@gmail.com>
2024-01-09 22:23:00 +0900

    fix: 폼 모아보기 api 수정

--- controller/formAllUser.js
+++ controller/formAllUser.js
@@ -1,22 +1,22 @@
-const { pg } = require('../models/database');
+const { Survey } = require('../models');
 
 const getUserSurveys = async (req, res) => {
   try {
     const userId = req.params.id;
 
-  
-    const queryResult = await pg.query(
-      'SELECT id, title, main_image_url, created_at FROM "Survey" WHERE user_id = $1', 
-      [userId],
-    );
-    const surveys = queryResult.rows;
+    // Survey 모델을 이용하여 해당 사용자가 생성한 템플릿을 조회합니다.
+    const surveys = await Survey.findAll({
+      where: { userId: userId },
+      attributes: ['id', 'title', 'mainImageUrl', 'createdAt'],
+    });
 
-    const countResult = await pg.query('SELECT COUNT(*) FROM "Survey" WHERE user_id = $1', [userId]);
-    const totalCount = countResult.rows[0].count;
+    // 해당 사용자가 생성한 템플릿의 총 개수를 조회합니다.
+    const totalCount = await Survey.count({ where: { userId: userId } });
 
-    // 결과 반환
-    res.json({ surveys: surveys, total_count: totalCount });
+    // 결과를 반환합니다.
+    res.json({ surveys, total_count: totalCount });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: '에러 발생', error: error.message });
   }
 };
