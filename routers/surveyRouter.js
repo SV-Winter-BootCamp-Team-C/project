738d26510af37d1722aef85c7f95f99ebb539ab7
HeeSeon Chang <jadej9846@gmail.com>
2024-01-10 20:31:25 +0900

    feat: 설문 내용 저장 api

--- routers/surveyRouter.js
+++ routers/surveyRouter.js
@@ -5,11 +5,13 @@ const surveyModifyController = require('../controller/surveyModify');
 const surveyAllUserController = require('../controller/formAllUser');
 const surveyGetController = require('../controller/surveyContentRead');
 const findAnsweredSurvey = require('../controller/surveyView');
+const surveyAnswerController = require('../controller/answerSave');
 
 router.post('/', surveyController.createSurveyWithQuestionsAndChoices);
 router.put('/:id', surveyModifyController.ModifySurveyWithQuestionsAndChoices);
 router.get('/:id/forms', surveyAllUserController.getUserSurveys);
 router.get('/:id/join', findAnsweredSurvey.findAnsweredSurvey);
 router.get('/:id', surveyGetController.getSurveyById);
+router.post('/:id', surveyAnswerController.createAnswer);
 
 module.exports = router;
