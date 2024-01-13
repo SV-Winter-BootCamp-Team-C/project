7a04b021084e83b397df4d3196a159adb79147d9
EunJu JO <dmswnehf@naver.com>
2024-01-13 23:05:17 +0900

    feat: AuthStore에 userId 추가

--- src/store/AuthStore.tsx
+++ src/store/AuthStore.tsx
@@ -2,18 +2,22 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface AuthStoreProps {
+  userId?: number;
   isLoggedIn: boolean;
+  setUserId: (userId: number) => void;
   setLoginStatus: (status: boolean) => void;
 }
 
 export const useAuthStore = create<AuthStoreProps>()(
   persist(
     (set) => ({
+      userId: undefined,
       isLoggedIn: false,
+      setUserId: (userId: number) => set({ userId }),
       setLoginStatus: (status: boolean) => set({ isLoggedIn: status }),
     }),
     {
-      name: 'isLoggedIn',
+      name: 'auth',
       storage: createJSONStorage(() => sessionStorage),
     },
   ),
