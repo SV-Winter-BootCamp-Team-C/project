6afe725a3829da87ce9dbc0d51c9a7fe951474a3
HeeSeon Chang <jadej9846@gmail.com>
2024-01-15 21:25:02 +0900

    feat: 이미지 검색 api 연동

--- controller/getImageBySearch.js
+++ controller/getImageBySearch.js
@@ -1,15 +1,10 @@
-// 필요한 모듈 임포트
-const express = require('express');
 const axios = require('axios');
-
-// Express 및 Multer 설정
-const app = express();
 
 // Pexels API 키 설정
 const PEXELS_API_KEY = process.env.PEXELS_API_KEY;
 
 // 이미지 검색 API 라우트
-app.get('/search/:query', async (req, res) => {
+const getImageByAPI = async (req, res) => {
   try {
     const response = await axios.get(
       `https://api.pexels.com/v1/search?query=${req.params.query}`,
@@ -19,12 +14,21 @@ app.get('/search/:query', async (req, res) => {
         },
       },
     );
-    const responseImageURL = response.data.querySelector('page');
 
-    console.log(responseImageURL);
+    // 각 이미지에서 'id'와 'src.original' 필드만 추출하여 새 배열 생성
+    const photos = response.data.photos.map((photo) => ({
+      id: photo.id,
+      original: photo.src.original,
+    }));
 
-    res.json(responseImageURL);
+    // 필요한 데이터만 포함된 새로운 응답 객체 생성
+    const modifiedResponse = {
+      photos: photos,
+    };
+
+    res.json(modifiedResponse);
   } catch (error) {
     res.status(500).send(error.toString());
   }
-});
+};
+module.exports = { getImageByAPI };
