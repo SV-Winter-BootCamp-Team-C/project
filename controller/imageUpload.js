59ff0df856c68e94354cc5f6c98fbdebda0289cb
ssojungg <kawy.sojung@gmail.com>
2024-01-16 21:11:46 +0900

    imageUpload.js 로 파일 이름 변경

--- controller/imageUpload.js
+++ controller/imageUpload.js
@@ -1,4 +1,8 @@
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} = require('@aws-sdk/client-s3');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
@@ -35,6 +39,59 @@ async function uploadFileToS3(file) {
   }
 }
 
+// S3에서 파일 삭제
+async function deleteFileFromS3(url) {
+  const bucket = process.env.AWS_BUCKET;
+  const key = url.split(
+    `https://${bucket}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/`,
+  )[1];
+
+  const params = {
+    Bucket: bucket,
+    Key: key,
+  };
+
+  try {
+    const command = new DeleteObjectCommand(params);
+    await s3Client.send(command);
+    console.log('File deleted successfully');
+  } catch (error) {
+    console.error('Error deleting file from S3:', error);
+    throw error;
+  }
+}
+
+async function updateFileOnS3(newFile) {
+  try {
+    // 새 파일 업로드
+    if (newFile) {
+      // 새로운 파일명 생성
+      const timestamp = Date.now();
+      const filename = `uploads/survey-images/${timestamp}_${newFile.originalname}`;
+
+      const params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: filename,
+        Body: newFile.buffer,
+        ACL: 'public-read-write',
+        ContentType: newFile.mimetype,
+      };
+
+      const command = new PutObjectCommand(params);
+      await s3Client.send(command);
+
+      const newUrl = `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${filename}`;
+      console.log(`새 이미지 업로드: ${newUrl}`);
+      return newUrl;
+    }
+
+    return null;
+  } catch (error) {
+    console.error('이미지 업데이트 오류:', error.message);
+    throw error;
+  }
+}
+
 // multer-s3 설정
 const uploadS3 = multer({
   storage: multerS3({
@@ -65,4 +122,4 @@ const uploadS3 = multer({
   { name: 'imageUrl', maxCount: 10 }, // 클라이언트에서 최대 10개의 이미지를 배열 형태로 보낼 수 있다고 가정
 ]);
 
-module.exports = { uploadS3, uploadFileToS3 };
+module.exports = { uploadS3, uploadFileToS3, deleteFileFromS3, updateFileOnS3 };
