50566213e746e623266ae479a62176a380c74669
ssojungg <kawy.sojung@gmail.com>
2024-01-16 20:33:02 +0900

    fix: 메인 이미지 수정 성공
    질문 이미지 수정 실패

--- controller/surveyModify.js
+++ controller/surveyModify.js
@@ -1,150 +1,133 @@
 const { sequelize } = require('../models');
-const { Survey, Question, Choice } = require('../models');
+const { Survey, Question, Choice, Answer } = require('../models');
 const {
   uploadFileToS3,
   //deleteFileFromS3,
   updateFileOnS3,
-} = require('./imageUpload');
+} = require('./imageUpload'); // 필요에 따라 경로를 조정하세요
 
 const ModifySurveyWithQuestionsAndChoices = async (surveyData, res) => {
-  const {
-    id: surveyId,
-    userId,
-    title,
-    description,
-    open,
-    font,
-    color,
-    buttonStyle,
-    mainImageUrlFile, // mainImageUrl을 파일 객체로 받습니다.
-    deadline,
-    questions,
-  } = surveyData;
-
-  let t;
-
   try {
-    console.log('Survey data:', surveyData);
+    const {
+      id: surveyId,
+      userId,
+      title,
+      description,
+      open,
+      font,
+      color,
+      buttonStyle,
+      mainImageUrl,
+      deadline,
+      questions,
+      //mainImageFile, // 메인 이미지 파일
+      questionImageFiles, // 질문 이미지 파일 배열
+    } = surveyData;
+
+    // 설문 조사의 소유권과 권한을 확인
     const survey = await Survey.findByPk(surveyId);
     if (!survey || survey.userId !== userId) {
-      return res.status(404).json({
-        message: userId + '는 설문을 작성한 사람이 아닙니다.',
-        resultCode: 403,
-      });
+      return res
+        .status(404)
+        .json({ message: userId + '는 설문을 작성한 사람이 아닙니다.' });
     }
 
-    // 접근 권한이 있는지 확인
     if (survey.userId !== userId || open) {
-      return res.status(403).json({
-        message: '접근 및 수정 권한이 없습니다.',
-        resultCode: 403,
-      });
+      return res.status(403).json({ message: '접근 및 수정 권한이 없습니다.' });
     }
 
-    // // 답변이 있는지 확인
-    // const answersCount = await Answer.count({
-    //   where: {
-    //     userId: userId,
-    //   },
-    // });
-
-    // if (answersCount > 0) {
-    //   return res.status(403).json({
-    //     message: '이미 답변이 있는 설문은 수정할 수 없습니다.',
-    //     resultCode: 403, // Modify this result code as needed
-    //   });
-    // }
-
-    // 트랜잭션 시작
-    t = await sequelize.transaction();
-
-    // mainImageUrl 업데이트 (새로운 이미지가 제공된 경우에만)
-    let newMainImageUrl;
-    if (mainImageUrlFile) {
-      newMainImageUrl = await updateFileOnS3(
-        survey.mainImageUrl,
-        mainImageUrlFile,
-      );
-    } else {
-      newMainImageUrl = survey.mainImageUrl; // 기존 이미지 유지
+    // 기존 답변의 존재 여부 확인
+    const answersCount = await Answer.count({ where: { userId: userId } });
+    if (answersCount > 0) {
+      return res
+        .status(403)
+        .json({ message: '이미 답변이 있는 설문은 수정할 수 없습니다.' });
     }
 
-    await survey.update(
-      {
-        title,
-        description,
-        open,
-        font,
-        color,
-        buttonStyle,
-        mainImageUrl: newMainImageUrl,
-        deadline,
-      },
-      { transaction: t },
-    );
+    await sequelize.transaction(async (t) => {
+      console.log('트랜잭션 시작');
 
-    // 기존 질문들을 찾아서 삭제
-    await Question.destroy({
-      where: { surveyId: surveyId },
-      transaction: t,
-      force: true,
-    });
+      // 메인 이미지 업로드/업데이트 처리
+  let updatedMainImageUrl = mainImageUrl;
+  if (surveyData.mainImageFile) { // 클라이언트에서 mainImageFile로 파일을 받고 있다고 가정
+    console.log('메인 이미지 업로드 중');
+    // S3에 파일을 업로드하고 URL을 업데이트합니다.
+    updatedMainImageUrl = await uploadFileToS3(surveyData.mainImageFile);
+    console.log('메인 이미지 업로드 완료: ', updatedMainImageUrl);
+  }
 
-    for (const questionData of questions) {
-      let questionImageUrl = '';
-      if (questionData.imageFile) {
-        questionImageUrl = await uploadFileToS3(questionData.imageFile);
-      }
 
-      const question = await Question.create(
+      // 설문 정보 업데이트
+      console.log('설문 정보 업데이트 중');
+      await survey.update(
         {
-          surveyId: surveyId,
-          type: questionData.type,
-          content: questionData.content,
-          imageUrl: questionImageUrl,
+          title,
+          description,
+          open,
+          font,
+          color,
+          buttonStyle,
+          mainImageUrl: updatedMainImageUrl,
+          deadline,
         },
         { transaction: t },
       );
+      console.log('설문 정보 업데이트 완료');
 
-      // 선택지 추가
-      if (
-        ['CHECKBOX', 'MULTIPLE_CHOICE', 'DROPDOWN'].includes(questionData.type)
-      ) {
-        for (const choice of questionData.choices) {
-          await Choice.create(
-            {
-              questionId: question.id, // 여기서 question.id를 사용합니다
-              option: choice.option,
-            },
-            { transaction: t },
-          );
+      // 기존 질문들 삭제 후 재생성
+      console.log('기존 질문 삭제 중');
+      await Question.destroy({
+        where: { surveyId: surveyId },
+        transaction: t,
+        force: true,
+      });
+      console.log('기존 질문 삭제 완료');
+
+      for (const [index, question] of questions.entries()) {
+        console.log(`새로운 질문 생성 중 ${index + 1}`);
+        // 새로운 질문 생성
+        const imageUrl =
+          questionImageFiles && questionImageFiles[index]
+            ? await uploadFileToS3(questionImageFiles[index])
+            : null;
+        console.log(`질문 이미지 업로드 중 ${index + 1}: `, imageUrl);
+
+        const newQuestion = await Question.create(
+          {
+            surveyId,
+            type: question.type,
+            content: question.content,
+            imageUrl: imageUrl,
+          },
+          { transaction: t },
+        );
+        console.log(`새로운 질문 생성 완료 ${index + 1}`);
+
+        // 필요한 경우 선택지 추가
+        if (
+          ['CHECKBOX', 'MULTIPLE_CHOICE', 'DROPDOWN'].includes(question.type)
+        ) {
+          for (const choice of question.choices) {
+            await Choice.create(
+              {
+                questionId: newQuestion.id,
+                option: choice.option,
+              },
+              { transaction: t },
+            );
+          }
         }
       }
-    }
 
-    // 설문 업데이트 시간 기록 및 트랜잭션 커밋
-    await survey.update({ updatedAt: new Date() }, { transaction: t });
-    await t.commit();
-    const updatedSurvey = await Survey.findByPk(surveyId);
-    console.log('Survey Update Result:', updatedSurvey.toJSON());
-
-    res.status(200).json({
-      message: '설문 수정에 성공하였습니다.',
-      survey: updatedSurvey,
+      // 설문 업데이트 시간 기록
+      await survey.update({ updatedAt: new Date() }, { transaction: t });
     });
+
+    const updatedSurvey = await Survey.findByPk(surveyId);
+    res.status(200).json({ message: '설문 수정 완료', updatedSurvey });
   } catch (error) {
     console.error('설문 수정 오류:', error.message);
-
-    // 트랜잭션 롤백
-    if (t) {
-      await t.rollback();
-    }
-
-    res.status(400).json({
-      message: '설문을 수정하는데 실패하였습니다.',
-      resultCode: 400,
-      error: error.message,
-    });
+    res.status(404).json({ message: '설문 수정 오류', error: error.message });
   }
 };
 
