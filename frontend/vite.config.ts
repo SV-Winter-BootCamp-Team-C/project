9617f0c5d1e409daab4cc3731bdbc97496954a57
EunJu JO <dmswnehf@naver.com>
2024-01-19 04:26:19 +0900

    fix: 프론트 포트 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    port: 3000,
+    port: 3001,
     host: true,
   },
   resolve: {
