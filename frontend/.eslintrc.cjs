fbc21651395bfc714ae6a0666a3122ffee76a152
Gildong <alsldl34@naver.com>
2024-01-24 05:20:34 +0900

    design: toggleSwitch 추가

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -38,6 +38,12 @@ module.exports = {
     'react/require-default-props': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor'],
+      },
+    ],
     'no-console': 'off',
     'no-alert': 'off',
     'import/extensions': [
