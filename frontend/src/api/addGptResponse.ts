eb748e0805d379145040eb1c447f76fa7c80f31d
overdelier <plum132132@gmail.com>
2024-01-24 15:53:51 +0900

    [fix] gpt 메서드 get에서 post로 변경.

--- frontend/src/api/addGptResponse.ts
+++ frontend/src/api/addGptResponse.ts
@@ -3,9 +3,10 @@ import { restFetcher } from '../queryClient';
 
 export const addGptResponseAPI = async (request: GptRequest) => {
   const response = await restFetcher({
-    method: 'GET',
+    method: 'POST',
     path: '/gpt-prompt',
     body: request,
   });
+  console.log(response);
   return response;
 };
