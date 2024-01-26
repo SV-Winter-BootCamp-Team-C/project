85e39854d1abbd9cab243033f5bf206beacbe205
overdelier <plum132132@gmail.com>
2024-01-26 20:06:57 +0900

    fix:잘못된 양식이어도 답하는 코드 추가, 프롬프트 수정

--- backend/gpt/gptAPI.js
+++ backend/gpt/gptAPI.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const { OpenAI } = require('openai');
+const choice = require('../models/choice');
+const question = require('../models/question');
 const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_KEY,
 });
+
 async function callChatGPT(prompt) {
   try {
     const response = await openai.chat.completions.create({
@@ -26,14 +29,14 @@ const getGptReponse = async (req, res) => {
     console.assert(req.body.description != undefined, 'error: description is undefined.');
     console.assert(req.body.type != undefined, 'error: type is undefined.');
     //make prompt
+    //문자열 합 연산을 여러번 하면 느리니까, 나중에 stringbuilder같은 걸로 해보기.
     let prompt = req.body.title + '라는 제목의 설문지를 만들려고 하는 데,';
-    //제목 + type
     prompt +=
       req.body.description +
       '이건 설문지의 설명이야. 설명은 응답할 때는 넣지 말아줘.';
     prompt +=
       req.body.content +
-      '(이 문구랑은 최대한 단어는 다르고 의미는 비슷하게)라는 내용을 묻는';
+      '라는 내용을 묻는 이 문구랑은 최대한 단어는 다르고 의미는 비슷한'
     switch (req.body.type) {
       case 'MULTIPLE_CHOICE':
         prompt += '객관식 (선택지가 4개 있어) 문항을 한 개만 만들어줘';
@@ -58,10 +61,13 @@ const getGptReponse = async (req, res) => {
         break;
     }
     prompt +=
-      '너의 답을 (문항: output\n선택지1: output\n선택지2: output\n선택지3: output\n선택지4: output) 이 양식에 맞추고, output 위치에 답을 넣어서 보내줘. 그리고 선지의 기호는 반드시 : 로 해줘. 마지막으로, 문항과 선지 외에는 응답에 넣지마.';
-    prompt += '예를 들어줄 게, 형식만 참고하고 내용은 참고하지 마, 예시1) 문항:어떤 음식을 주로 먹으시나요?\n선택지1:밥\n선택지2:스파게티\n선택지3:빵\n선택지4:죽,\n 예시2) 문항:그 언어를 사용하시는 이유가 무엇인가요?\n선택지1:설계가 좋아서\n선택지2:언어의 기본툴이 좋아서\n선택지3:대세언어라서\n선택지4:그 언어만 지원하는 기능이 있어서';
+      '너의 답을 (문항: output\n선택지1: output\n선택지2: output\n선택지3: output\n선택지4: output) 이 양식에 맞추고, output 위치에 답을 넣어서 보내줘.';
+    prompt += '예를 들어줄 게 예시는 응답할 때는 넣지 마. 예시1) 문항:어떤 음식을 주로 먹으시나요?\n선택지1:밥\n선택지2:스파게티\n선택지3:빵\n선택지4:죽,\n 예시2) 문항:그 언어를 사용하시는 이유가 무엇인가요?\n선택지1:설계가 좋아서\n선택지2:언어의 기본툴이 좋아서\n선택지3:대세언어라서\n선택지4:그 언어만 지원하는 기능이 있어서';
+
+    console.log(prompt);
     const response = await callChatGPT(prompt);
 
+    console.log(response.choices[0].message.content);
     console.assert(response != null, 'error: response is null');
     // GPT 모델로부터의 응답 처리 (일단 문항 + option으로)
     const lines = response.choices[0].message.content.split('\n');
@@ -75,15 +81,9 @@ const getGptReponse = async (req, res) => {
         }
         questIndex++;
       }
-      const question = lines[questIndex]
-        .substring(
-          lines[questIndex].indexOf(' ') + 1,
-          lines[questIndex].length,
-        )
-        .trimStart();
-      console.log(question);
+      const question = lines[questIndex].substring(lines[questIndex].indexOf(' ') + 1, lines[questIndex].length).trimStart();
 
-      const options = new Array(4);
+      const choices = new Array(4);
       var zeroLengthCount = 0;
       var i = questIndex + 1;
       var count = 0;
@@ -93,23 +93,21 @@ const getGptReponse = async (req, res) => {
         if (lines[i] != undefined && lines[i].length != 0) {
           var index = lines[i].indexOf(':');
           if (index != -1) {
-            options[count] = lines[i]
+            choices[count] = { option:lines[i]
               .substring(index + 1, lines[i].length)
-              .trimStart();
+              .trimStart() };
             ++count;
           }
         }
         ++i;
       }
 
-      const choices = new Array(4);
-      for (i = 0; i < 4; ++i) {
-        choices[i] = { option: options[i] };
-      }
       res.status(200).json({ content: question, choices: choices });
     } catch(error) {
       console.log('Invalid format\n',response);
-      res.status(202);
+      console.log(question);
+      console.log(choices);
+      res.status(202).json({ content: question, choices: { option:"GPT가 제대로 된 응답을 하지 않았습니다.", option:"다시 해주시길 바랍니다. -- _ _"}});
     }
   } catch (error) {
     console.error(error);
